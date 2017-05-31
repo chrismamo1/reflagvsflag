4c207025731562578e2733b01ecf2a030d2086d2
chrismamo1 <christophermcalpine@gmail.com>
2017-05-31 17:35:43 -0600

    maybe try running the server on the AWS node's "private IP"?" *shrugs*

--- main.go
+++ main.go
@@ -552,7 +552,7 @@ func main() {
 
     srv := &http.Server{
         Handler:        r,
-        Addr:           "127.0.0.1:80",
+        Addr:           "172.31.76.179:80",
         WriteTimeout:   15 * time.Second,
         ReadTimeout:    15 * time.Second,
     }
