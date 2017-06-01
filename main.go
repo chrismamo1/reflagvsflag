1bebe1ffbaddd4db616a24404baede6047b937ea
chrismamo1 <christophermcalpine@gmail.com>
2017-05-31 20:10:35 -0600

    fix a syntax issue

--- main.go
+++ main.go
@@ -108,11 +108,12 @@ func VoteHandler(db *sql.DB, resps chan things.IDPair) func(http.ResponseWriter,
 
         user := users.GetByAddr(db, req.RemoteAddr)
         query := `
-        CASE WHEN (EXISTS(SELECT * FROM EXPOSURE WHERE "user" = $1 AND image = $2)) THEN
-            (UPDATE exposure SET heat = heat + 1 WHERE "user" = $3 AND image = $4)
-        ELSE
-            (INSERT INTO exposure ("user", image, heat) VALUES ($5, $6, 1))
-        END
+        SELECT
+            CASE WHEN (EXISTS(SELECT * FROM EXPOSURE WHERE "user" = $1 AND image = $2)) THEN
+                (UPDATE exposure SET heat = heat + 1 WHERE "user" = $3 AND image = $4)
+            ELSE
+                (INSERT INTO exposure ("user", image, heat) VALUES ($5, $6, 1))
+            END
         `
         if _, err := db.Exec(query, user.Id, winner, user.Id, winner, user.Id, winner); err != nil {
             log.Fatal("problem updating/modifying exposure table: ", err)
