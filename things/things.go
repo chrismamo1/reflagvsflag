5859cfb53b1bc49ede2f5010fe8b2c2f77c329a5
chrismamo1 <christophermcalpine@gmail.com>
2017-06-01 19:28:07 -0600

    hopefully fix showing country names and general flag rendering

--- things/things.go
+++ things/things.go
@@ -64,7 +64,7 @@ func render(thing Thing, root string, maxWidth int, maxHeight int) string {
         } else {
             format = `
                 <div>
-                    <h5 style="text-align: center">{{.Name}}</h5>
+                    <h5>{{.Name}}</h5>
                     <img
                         style='max-width: {{.MaxWidth}}px; max-height: {{.MaxHeight}}px; box-shadow: 0px 0px 5px black'
                         src='{{.Path}}'>
@@ -75,7 +75,7 @@ func render(thing Thing, root string, maxWidth int, maxHeight int) string {
     } else {
         format = `
             <div>
-                <h5 style="text-align: center">{{.Name}}</h5>
+                <h5>{{.Name}}</h5>
                 <img
                     style='max-width: {{.MaxWidth}}px; max-height: {{.MaxHeight}}px; box-shadow: 0px 0px 5px black'
                     src='{{.Path}}'>
