aaa430f43919740691b85fd181fdaedf69f1e67e
chrismamo1 <christophermcalpine@gmail.com>
2017-06-02 21:31:34 -0600

    sort requests by priority first

--- comparisonScheduler/comparisonScheduler.go
+++ comparisonScheduler/comparisonScheduler.go
@@ -171,8 +171,7 @@ func (this *Scheduler) NextRequest(user users.User) *things.IDPair {
         WHERE ("user" = $1 AND (image = fst OR image = snd))
         GROUP BY ROLLUP (id, fst, snd, priority, placement)
         ORDER BY
-            priority DESC,
-            s_heat ASC,
+            (s_heat - COALESCE(priority, 0)) ASC,
             placement ASC
         LIMIT 1;
     `
