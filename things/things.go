fb17c363936ddb09f1d94f6889dd01850d0a8bdb
chrismamo1 <christophermcalpine@gmail.com>
2017-06-02 18:07:31 -0600

    tinker with the styles

--- things/things.go
+++ things/things.go
@@ -63,7 +63,7 @@ func render(thing Thing, root string, maxWidth int, maxHeight int) template.HTML
             `
         } else {
             format = `
-                <div style="padding: 5px; margin-top: -50%; height: 100%">
+                <div style="padding: 5px; height: 80%; top: 10%; width: 100%">
                     <center>
                         <h3>{{.Name}}</h3>
                     </center>
@@ -76,7 +76,7 @@ func render(thing Thing, root string, maxWidth int, maxHeight int) template.HTML
         }
     } else {
         format = `
-            <div style="padding: 5px">
+            <div style="padding: 5px; height: 80%; top: 10%; width: 100%">
                 <center>
                     <h3>{{.Name}}</h3>
                 </center>
