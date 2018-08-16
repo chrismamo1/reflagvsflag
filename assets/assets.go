811d18164014adce9b1f91c10271e8749bbcaa3c
chrismamo1 <christophermcalpine@gmail.com>
2018-08-16 12:32:43 -0600

    salt filenames and add a couple of comments

--- assets/assets.go
+++ assets/assets.go
@@ -5,10 +5,12 @@ import (
 	"crypto/md5"
 	"fmt"
 	"log"
+	"math/rand"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -31,7 +33,10 @@ func UploadImage(file multipart.File, header *multipart.FileHeader) string {
 	cfg := aws.NewConfig().WithRegion("us-east-1").WithCredentials(creds)
 	svc := s3.New(session.New(), cfg)
 
-	buffer := make([]byte, 2*(1<<20))
+	/// read the multipart file into a byte array and record its size
+	// set the buffer capacity to 4MB
+	bufferCap := 4 * (1 << 20)
+	buffer := make([]byte, bufferCap)
 	var nRead int64 = 0
 	var n int = 1
 	for n > 0 {
@@ -50,8 +55,20 @@ func UploadImage(file multipart.File, header *multipart.FileHeader) string {
 		fileType = "image/svg+xml"
 	}
 
+	/// take a hash of the image data and use it for the image path
+	// just using the image data has fucky behaviour when people try to upload
+	// identical images (some historical countries have had flags that are/were
+	// the same solid color).
 	h := md5.New()
 	h.Write(buffer[0:nRead])
+	// we fix this with a salt
+	rand.Seed(time.Now().UTC().UnixNano())
+	saltLen := 8 // 8 bytes should be enough
+	salt := make([]byte, saltLen)
+	for i, _ := range salt {
+		salt[i] = byte(rand.Uint32() % 256)
+	}
+	h.Write(salt)
 	checkSum := fmt.Sprintf("%x", h.Sum(nil))
 
 	path := "/user-flags/" + checkSum + "_" + header.Filename
